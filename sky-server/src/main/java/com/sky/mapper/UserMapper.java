a9f3192a32de9fde44fbacedb04f32ab11a58fb9
HNUrookie1 <1964573432@qq.com>
2025-07-17 19:10:34 +0800

    苍穹外卖day08代码--用户下单、订单支付代码开发完成

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -25,4 +25,12 @@ public interface UserMapper {
     @Insert("insert into user (id, openid, name, phone, sex, id_number, avatar, create_time) "
     + "values (#{id},#{openid},#{name},#{phone},#{sex},#{idNumber},#{avatar},#{createTime})")
     void insert(User user);
+
+    /**
+     * 通过id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
